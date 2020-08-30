743b28b0fe285225fdb5f8949a8693d5b125c628
cingaldi <carmine.ingaldi@glovoapp.com>
2020-08-30 12:20:39 +0200

    fix: adding missing feature in traning DS

--- scripts/main.py
+++ scripts/main.py
@@ -3,9 +3,9 @@ import pandas as pd
 df_train = pd.read_csv("data/dataset/train.csv")
 df_test = pd.read_csv("data/dataset/test.csv")
 
-
-X = df_train[["Pclass" , "Age" , "SibSp" , "Parch" , "Fare"]]
+X = df_train[['Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']]
 Y = df_train["Survived"]
+
 
 print("> Train Dataset - Counting missing values over {} examples".format(len(X)))
 print(X.isnull().sum())
